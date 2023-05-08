50e6f497812494c3fa7069bc972bbab13b324297
daniel.corner <danielcorner7@gmail.com>
2023-05-07 21:47:59 -0400

    add collections to api

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,22 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="p-6 flex justify-between items-center">
-      <Link href="/">
+      <Link href="/" className="hover_zoom">
         <div className="text-white text-2xl font-bold">RASH PL.</div>
       </Link>
       <nav>
         <ul className="flex space-x-6">
-          <li>
+          <li className="click_padding hover_underline">
             <Link href="/sportswear">
               <span className="text-white">스포츠의류</span>
             </Link>
           </li>
-          <li>
+          <li className="click_padding hover_underline">
             <Link href="/beachwear_rashguard">
               <span className="text-white">비치웨어/래쉬가드</span>
             </Link>
           </li>
-          <li>
+          <li className="click_padding hover_underline">
             <Link href="/swimwear_rashguard">
               <span className="text-white">수영복/래쉬가드</span>
             </Link>
